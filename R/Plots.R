13601fb230930706c6a5eabda3ebe0e2f12814d8
Stefan Lang <Stefan.Lang@med.lu.se>
2016-04-15 07:48:49 +0200

    Modifications to define the bean and violine plot data for one call only.

--- R/Plots.R
+++ R/Plots.R
@@ -352,29 +352,35 @@ setMethod('FACS.heatmap', signature = c ('Rscexv'),
 #' @param x the Rscexv object
 #' @param groups.n  TEXT MISSING
 #' @param clus  TEXT MISSING
-#' @param boot  TEXT MISSING default= 1000
 #' @param plot.neg  TEXT MISSING default=FALSE
 #' @param mv  TEXT MISSING default=-20
 #' @param subpath the path to write the figures to (default = '')
+#' @param col a colour vector for colouring the violines (default=NULL  -> rainbow )
 #' @title description of function plot.violines
 #' @export 
 setGeneric('plot.violines', ## Name
-		function ( x, groups.n, clus, boot = 1000, plot.neg=FALSE, mv=-20, subpath='') { ## Argumente der generischen Funktion
+		function ( x, groups.n, clus, plot.neg=FALSE, mv=-20, subpath='', names=NULL, col=NULL) { ## Argumente der generischen Funktion
 			standardGeneric('plot.violines') ## der Aufruf von standardGeneric sorgt für das Dispatching
 		}
 )
 
 setMethod('plot.violines', signature = c ('Rscexv'),
-		definition = function ( x, groups.n, clus, boot = 1000, plot.neg=FALSE, mv=-20, subpath='') {
+		definition = function ( x, groups.n, clus,  plot.neg=FALSE, mv=-20, subpath='', names=NULL, col=NULL) {
 			ma <- NULL
 			if ( x@wFACS ){
 				ma <- t(cbind( x@usedObj[['for.plot']], x@facs ))
 			}else{
 				ma <- t(x@usedObj[['for.plot']])
 			}
-			
+			if ( subpath != '' ){
+				system( paste('mkdir',file.path(x@outpath,subpath ) ) )
+			}
 			n <- rownames(ma)
-			cols = rainbow( groups.n )
+			if ( is.null(col)){
+				col = rainbow( groups.n )
+			}
+			cols = col
+			
 			s <-  split(seq(ncol(ma)), clus)
 			for ( i in 1:nrow( ma ) ) {
 				fn <- file.path(x@outpath,subpath, n[i] )
@@ -388,6 +394,9 @@ setMethod('plot.violines', signature = c ('Rscexv'),
 							} ) ))
 				if ( ! plot.neg ){
 					lila = lapply( lila, function(x) { x[which(x != mv)] = NA; x } )
+				}
+				if ( ! is.null(names) && length(names) == length(lila$names) ){
+					lila$names= paste( names, lila$names)
 				}
 				names(lila)[1]= 'x'
 				lila$col= cols
@@ -608,20 +617,21 @@ setMethod('col4bean', signature = c ('character'),
 #' @param x the Rscexv object
 #' @param groups.n  TEXT MISSING
 #' @param clus  TEXT MISSING
-#' @param boot  TEXT MISSING default= 1000
 #' @param plot.neg  TEXT MISSING default=TRUE
 #' @param mv  TEXT MISSING default=-20
 #' @param subpath the subpath for the plots (default = '')
+#' @param names a vector of grou names (default NULL)
+#' @param col a colour vector for colouring the beans (default=NULL -> rainbow )
 #' @title description of function plot.beans
 #' @export 
 setGeneric('plot.beans', ## Name
-		function ( x, groups.n, clus, boot = 1000, plot.neg=TRUE, mv=-20, subpath='' ) { ## Argumente der generischen Funktion
+		function ( x, groups.n, clus,  plot.neg=TRUE, mv=-20, subpath='', names=NULL, col=NULL ) { ## Argumente der generischen Funktion
 			standardGeneric('plot.beans') ## der Aufruf von standardGeneric sorgt für das Dispatching
 		}
 )
 
 setMethod('plot.beans', signature = c ('Rscexv'),
-		definition = function ( x, groups.n, clus, boot = 1000, plot.neg=TRUE, mv=-20, subpath='' ) {
+		definition = function ( x, groups.n, clus, plot.neg=TRUE, mv=-20, subpath='', names=NULL, col=NULL ) {
 			ma <- NULL
 			if ( x@wFACS ){
 				ma <- t(cbind( x@usedObj[['for.plot']], x@facs ))
@@ -629,8 +639,14 @@ setMethod('plot.beans', signature = c ('Rscexv'),
 				ma <- t(x@usedObj[['for.plot']])
 			}
 			n <- rownames(ma)
-			cols = col4bean(rainbow( groups.n ))	
+			if ( is.null(col)){
+				col = rainbow( groups.n )
+			}
+			cols = col4bean(col)	
 			s <-  split(seq(ncol(ma)), clus)
+			if ( subpath != '' ){
+				system( paste('mkdir',file.path(x@outpath,subpath ) ) )
+			}
 			for ( i in 1:nrow( ma ) ) {
 				#print (paste( 'plot.beans working on gene', n[i] ) )
 				fn <- file.path(x@outpath,subpath, n[i] )
@@ -646,6 +662,9 @@ setMethod('plot.beans', signature = c ('Rscexv'),
 							lila[[a]] = c(0)
 						}
 					}
+				}
+				if ( ! is.null(names) && length(names) == length(lila$names) ){
+					lila$names= paste( names, lila$names)
 				}
 				lila$main <- n[i]
 				lila$what <- c(1,1,0,1) ## not plot medain line
