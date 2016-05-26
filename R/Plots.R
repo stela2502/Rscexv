073a7558135e689b24084b53dfe1e67a25948c49
Stefan Lang <Stefan.Lang@med.lu.se>
2016-05-26 09:50:59 +0200

    New mergeComplexSampleGroupings

--- R/Plots.R
+++ R/Plots.R
@@ -706,10 +706,10 @@ setGeneric('mergeSampleGroupings', ## Name
 setMethod('mergeSampleGroupings', signature = c ('Rscexv'),
 		definition = function ( x, g1, g2, newName ){
 	if (length( table( x@samples[,g2] ) ) > 2) {
-		stop( "This function can only merge one comülex grouping with a yes/no information")
+		stop( "This function can only merge one complex grouping with a yes/no information")
 	}
 	if ( ! all.equal( names( table(x@samples[,g2] )), c('no','yes')) ){
-		stop( "This function can only merge one comülex grouping with a yes/no information")
+		stop( "This function can only merge one complex grouping with a yes/no information")
 	}
 	if(is.null( x@usedObj[['colorRange']] ) ){
 		x@usedObj[['colorRange']] <- list()
@@ -735,5 +735,44 @@ setMethod('mergeSampleGroupings', signature = c ('Rscexv'),
 	x@usedObj[['colorRange']][[ match( newName, names(x@usedObj[['colorRange']])) ]] <- interleave ( rainbow( max(as.numeric(x@samples[,g1]))), rainbow( max(as.numeric(x@samples[,g1])), s=0.5, v=0.5) )
 	x
 	}
+)
+
+#' @name mergeComplexSampleGroupings
+#' @aliases mergeComplexSampleGroupings,Rscexv-method
+#' @rdname mergeComplexSampleGroupings-methods
+#' @docType methods
+#' @description Create a new group and create shaded rainbow color shemata stored in the x@usedObj[['colorRange']] variable
+#' @param x the Rscexv object
+#' @param g1 the first grouping name (can be complex)
+#' @param g2 the second grouping name (can be complex too)
+#' @param newName the name of the new variable to create
+#' @title description of function mergeSampleGroupings
+#' @export 
+setGeneric('mergeComplexSampleGroupings', ## Name
+		function ( x,  g1, g2, newName ) { ## Argumente der generischen Funktion
+			standardGeneric('mergeComplexSampleGroupings') ## der Aufruf von standardGeneric sorgt für das Dispatching
+		}
+)
+
+setMethod('mergeComplexSampleGroupings', signature = c ('Rscexv'),
+		definition = function ( x, g1, g2, newName ){
+			if(is.null( x@usedObj[['colorRange']] ) ){
+				x@usedObj[['colorRange']] <- list()
+			}
+			newG <- factor(paste( as.vector(x@samples[, g1]), as.vector( x@samples[, g2]) , sep='_' ))
+			
+			if ( is.na(match( newName, colnames(x@samples))) ){
+				x@samples =cbind( x@samples, newName=newG )
+				colnames(x@samples)[ncol(x@samples)] = newName
+			}else{
+				x@samples[,newName] = newG
+			}
+			if ( is.na( match( newName, names(x@usedObj[['colorRange']]) ) ) ) {
+				x@usedObj[['colorRange']][[length(x@usedObj[['colorRange']])+1]] <- 1
+				names(x@usedObj[['colorRange']])[[length(x@usedObj[['colorRange']])]] = newName
+			}
+			x@usedObj[['colorRange']][[ match( newName, names(x@usedObj[['colorRange']])) ]] <-  rainbow( max(as.numeric(newG)) )
+			x
+		}
 )
 
