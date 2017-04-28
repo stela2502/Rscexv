938eec93e3e253bec4d25d45fbdbae44bd9d1bac
stefan <st.t.lang@gmx.de>
2017-04-28 11:27:40 +0200

    install.R now checks if libs are installed before installing them.

--- install.R
+++ install.R
@@ -1,7 +1,13 @@
-install.packages(c('devtools'),  repos='https://ftp.acc.umu.se/mirror/CRAN/')
-library(devtools)
+if (!library("devtools", quietly = TRUE,logical.return=TRUE )) {
+	install.packages(c('devtools'),  repos='https://ftp.acc.umu.se/mirror/CRAN/')
+	library(devtools)
+}
 source("https://bioconductor.org/biocLite.R")
 biocLite()
-install_github('RGLab/MAST')
-install_github('stela2502/RFclust.SGE')
+if (!library("MAST", quietly = TRUE,logical.return=TRUE )) {
+	install_github('RGLab/MAST')
+}
+if (!library("RFclust", quietly = TRUE,logical.return=TRUE )) {
+	install_github('stela2502/RFclust.SGE')
+}
 install()
